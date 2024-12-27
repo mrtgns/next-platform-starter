201b284c80c26ff30f6000a772d0207dc0384663
mrtgns <mgunes004@gmail.com>
2024-12-27 19:54:26 +0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -10,26 +10,7 @@ export function Footer() {
             <p className="text-sm mt-2">Online destek ve rehberlik için buradayız.</p>
           </div>
       
-          {/* Menü Linkleri */}
-          <div className="w-full md:w-1/3 mb-4 md:mb-0 text-center">
-            <ul className="flex justify-center md:justify-center space-x-4">
-              <li>
-                <a href="/hakkimizda" className="hover:text-white">
-                  Hakkımızda
-                </a>
-              </li>
-              <li>
-                <a href="/hizmetler" className="hover:text-white">
-                  Hizmetler
-                </a>
-              </li>
-              <li>
-                <a href="/iletisim" className="hover:text-white">
-                  İletişim
-                </a>
-              </li>
-            </ul>
-          </div>
+        
       
           {/* İletişim Bilgileri */}
           <div className="w-full md:w-1/3 text-center md:text-right">
