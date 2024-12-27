7801f86b37d403217279d12dd9f5858d2836a1b9
mrtgns <mgunes004@gmail.com>
2024-12-27 19:09:42 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="tr">
           <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+           
           >
             <Header/>
             {children}
