681591c97b3a515fcb86d00758733df94586d359
mrtgns <mgunes004@gmail.com>
2025-02-02 10:12:01 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
-};
-
-module.exports = nextConfig;
+    async headers() {
+      return [
+        {
+          source: '/(.*)', // Tüm yollar için geçerli olacak
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self'; img-src 'self' data: https:; script-src 'self' https://www.chatbase.co; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;"
+            },
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=63072000; includeSubDomains; preload'
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff'
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY'
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin'
+            },
+            {
+              key: 'Permissions-Policy',
+              value: 'geolocation=(), microphone=(), camera=()'
+            }
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
