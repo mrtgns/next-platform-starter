fe6fe77d167938480e5074d0254c3e4eafe64339
mrtgns <mgunes004@gmail.com>
2025-02-12 17:49:48 +0300

    vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
-};
-
-module.exports = nextConfig;
+    reactStrictMode: true,
+    swcMinify: true,
+    output: "standalone",
+    experimental: {
+      appDir: true
+    }
+  };
+  
+  module.exports = nextConfig;
